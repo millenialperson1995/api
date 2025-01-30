8529c339f9d77421cc2351473c24f52580665f52
millenialperson1995 <millenialperson1995@outlook.com.br>
2025-01-30 15:22:28 +0000

    new commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, BookmarkModule, UserModule],
+  imports: [AuthModule, BookmarkModule, UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I have signed up' }
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        }
+      });
+      return user;
+
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credencials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
     return { msg: 'I have signed in' }
   }
 }
+// aula 1:13:14
