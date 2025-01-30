cfb38050036bc6bbb70b3ff1139c20ad6990667f
millenialperson1995 <millenialperson1995@outlook.com.br>
2025-01-30 13:48:24 +0000

    up prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, BookmarkModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
